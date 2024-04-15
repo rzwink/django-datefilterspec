a18a52411ccdebd2a1da8138b99da07dc6e9fcd5
Robert Zwink <rzwink@users.noreply.github.com>
2024-04-15 10:09:18 -0400

    Update filter.py

--- daterange_filter/filter.py
+++ daterange_filter/filter.py
@@ -12,7 +12,12 @@ from django import forms
 from django.contrib import admin
 from django.db import models
 from django.utils.translation import ugettext as _
-from django.contrib.admin.templatetags.admin_static import static
+try:
+    # Django 2
+    from django.contrib.staticfiles.templatetags.staticfiles import static
+except ModuleNotFoundError:
+    # Django 3
+    from django.templatetags.static import static
 from django.conf import settings
 
 use_suit = 'DATE_RANGE_FILTER_USE_WIDGET_SUIT'
